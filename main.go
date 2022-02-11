de94fefb4ecd200b82a2d5a455be5a04b747d711
mortezaalizadeh66 <65837442+mortezaalizadeh66@users.noreply.github.com>
2022-02-11 17:09:49 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+func main() {
+	fmt.Println("Go Example")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
